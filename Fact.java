21d1c1536accd39cde6992400d68b1dfd21cb034
uppusruthi6250 <uppusruthi64@gmail.com>
2026-09-17 23:13:47 +0530

    added hello.java

---
+++ Fact.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Fact {
+
+    static int factorial(int n) {
+        int fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(factorial(n));
+    }
+}
